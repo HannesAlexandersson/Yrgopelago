4eca523e29a3d48a21ccd5a8759f6eed7f33eae6
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-12-09 18:27:58 +0100

    namechanged some php files
    only small change on some filenames to be more explainatory with the names

--- getAPIkey.php
+++ getAPIkey.php
@@ -8,7 +8,6 @@ $centralBankService = new CentralBankService();
 $api_key = $centralBankService->getApiKey($startcode);
 
 if ($api_key) {
-    // Save API key to a file
     file_put_contents('api_key.txt', $api_key);
     echo "API Key: $api_key saved successfully.";
 } else {
