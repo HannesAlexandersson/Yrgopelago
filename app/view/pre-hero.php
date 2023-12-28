d4c10c508918f9d92d8640b5fafc6ebfa5facd37
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-12-28 22:53:40 +0100

    added a pre hero with scrollreveal effect

---
+++ app/view/pre-hero.php
@@ -0,0 +1,35 @@
+
+<div class="bg-pre">
+  <div class="left-side-pre">
+
+    <section class="scroll-reveal pre-header-wrapper default-font">
+      <h1><span>Hotel Avalon</span></h1>
+      <p>
+        <span>
+          Where dreams are born, and time is never planned.
+          Come in, explore our hotel and let us make yours a reality.
+        </span>
+      </p>
+    </section>
+
+  </div>
+
+
+  <div class="right-side-pre">
+
+    <section class="img-grid-wrapper">
+
+      <div class="img-grid-top">
+        <img src="../../assets/images/hotel/topLeft.svg" alt="beach-hotel at night" class="img-grid-item">
+        <img src="../../assets/images/hotel/topMid.svg" alt="beach-hotel at night" class="img-grid-item">
+        <img src="../../assets/images/hotel/topRight.svg" alt="beach-hotel at night" class="img-grid-item">
+      </div>
+      <div class="img-grid-bottom">
+        <img src="../../assets/images/hotel/botLeft.svg" alt="beach with palms at night, sunset" class="img-grid-item">
+        <img src="../../assets/images/hotel/botRight.svg" alt="beach with palms at late sunset" class="img-grid-item">
+      </div>
+
+    </section>
+
+  </div>
+</div>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require __DIR__ . '/header.php';
 require __DIR__ . '/app/view/navbar.php';
 require __DIR__ . '/app/view/hero.php';
+require __DIR__ . '/app/view/pre-hero.php';
 require __DIR__ . '/app/view/home.php';
 require __DIR__ . '/app/view/rooms.php';
 require __DIR__ . '/app/view/features.php';
