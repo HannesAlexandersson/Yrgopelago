c5505a9b6ec85210f83ce4cc60cbd8445bcc7632
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-01-04 23:57:26 +0100

    changed some var names

--- preloader.php
+++ preloader.php
@@ -1,12 +1,12 @@
 <div class="preloader-container">
   <h1 class="preloader">
-      <span class="letter1 preloader-span">l</span>
-      <span class="letter2 preloader-span">o</span>
-      <span class="letter3 preloader-span">a</span>
-      <span class="letter4 preloader-span">d</span>
-      <span class="letter5 preloader-span">i</span>
-      <span class="letter6 preloader-span">n</span>
-      <span class="letter7 preloader-span">g</span>
+      <span class="letterL preloader-span">l</span>
+      <span class="letterO preloader-span">o</span>
+      <span class="letterA preloader-span">a</span>
+      <span class="letterD preloader-span">d</span>
+      <span class="letterI preloader-span">i</span>
+      <span class="letterN preloader-span">n</span>
+      <span class="letterG preloader-span">g</span>
     </h1>
   </div>
   <script>
