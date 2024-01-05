e81cb1a9f4c91058ceba971e06b10d0006c7ad80
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-01-05 17:34:43 +0100

    bring back the parallax cloud
    it went missing and i had to find it again

--- app/scripts/discount-paralax.js
+++ app/scripts/discount-paralax.js
@@ -1,8 +1,8 @@
 document.addEventListener('scroll', function() {
   let scrollPosition = window.scrollY;
-  document.querySelector('.discount-bg-beach').style.backgroundPositionY = -scrollPosition * 0.2 + 'px';
+  document.querySelector('.discount-bg-beach').style.backgroundPositionY = -scrollPosition * 0.15 + 'px';
   document.querySelector('.discount-bg-palms').style.backgroundPositionY = scrollPosition * 0.05 + 'px';
-  document.querySelector('.discount-bg-clouds').style.backgroundPositionY = -scrollPosition * 0.001 + 'px';
+  document.querySelector('.discount-bg-clouds').style.backgroundPositionY = -scrollPosition * 0.005 + 'px';
 
 
 });
